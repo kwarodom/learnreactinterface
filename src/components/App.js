c4f8fa574c75b458a1965e8f1f043334ef4f3f15
Warodom Khamphanchai <kwarodom@altotech.net>
2020-08-29 20:48:10 +0700

    4.1 Preparing our search components

--- src/components/App.js
+++ src/components/App.js
@@ -14,7 +14,9 @@ class App extends Component {
       myName: "Warodom",
       myAppointments: [],
       lastIndex: 0,
-      formDisplay: false
+      formDisplay: false,
+      orderBy: "petName",
+      orderDir: "asc"
     }
     this.deleteAppointment = this.deleteAppointment.bind(this);
     this.toggleForm = this.toggleForm.bind(this);
@@ -65,6 +67,25 @@ class App extends Component {
   }
 
   render() {
+
+    let order;
+    let filteredApts =  this.state.myAppointments;
+    if(this.state.orderDir === 'asc') {
+      order = 1;
+    } else {
+      order = -1;
+    }
+
+    filteredApts.sort((a,b) => {
+      if (a[this.state.orderBy].toLowerCase() < 
+          b[this.state.orderBy].toLowerCase()
+          ) {
+            return -1 * order;
+          } else {
+            return 1 * order;
+          }
+    });
+
     return (
       <main className="page bg-white" id="petratings">
         <div className="container">
@@ -77,7 +98,7 @@ class App extends Component {
                 addAppointment={this.addAppointment}
                 />
                 <SearchAppointments />
-                <ListAppointments appointments={ this.state.myAppointments }
+                <ListAppointments appointments={ filteredApts }
                 deleteAppointment={ this.deleteAppointment } />
               </div>
             </div>
